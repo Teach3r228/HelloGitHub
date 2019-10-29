7d0ccf7e9fcb37ddf48d6befb66a79bcd0689a6d
artemLaptop233223@outlook.com <artemLaptop233223@outlook.com>
2019-10-30 01:38:41 +0300

    Тестовый апдейт с ноута

--- ConsoleTestGitHub/Program.cs
+++ ConsoleTestGitHub/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleTestGitHub
             Console.WriteLine("Hello World!");
             Console.WriteLine("Первое изменение!"); 
             Console.WriteLine("Второе изменение!");
+            Console.WriteLine("Изменение на ноуте");
             Console.ReadKey();
         }
     }

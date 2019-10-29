fe919ac8549c287c43eea77d39f45c34a188ea42
Artem_Nagorichnyi <artemnagorichniy@yandex.ru>
2019-10-30 00:08:14 +0700

    Первое изменение

--- ConsoleTestGitHub/Program.cs
+++ ConsoleTestGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleTestGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Первое изменение!");
             Console.ReadKey();
         }
     }
